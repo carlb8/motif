b62237312a063a3be704661c02f408bc3901a6f5
carlb8 <ax206geek.v2@gmail.com>
2015-07-15 19:19:51 +0800

    Update edit.php
    Support GV OAUTH2.0

--- views/edit.php
+++ views/edit.php
@@ -1,4 +1,31 @@
 <?php
+/*
+This file is part of FreePBX motif module and derived from https://github.com/FreePBX/motif/raw/a899a286fa6af330371755325a209fcd4741bc95/functions.inc.php
+You can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License. 
+Copyright 2015 Carl B.
+Copyright 2013 Schmooze Com Inc.
+Copyright 2012 Andrew Nagy
+You should have received a copy of the GNU General Public License along with motif module.  If not, see <http://www.gnu.org/licenses/>.
+*/
+echo "<script type='text/javascript'>";
+echo "function open_auth_window(){";
+echo "var cid_element = document.getElementById('cid');";
+echo "var cid_value = cid_element.value;";
+echo "document.getElementById('tokencb').checked=false;";
+echo "window.open('https://accounts.google.com/o/oauth2/auth?client_id='+cid_value+'&scope=https://www.googleapis.com/auth/googletalk%20https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&response_type=code&redirect_uri=urn:ietf:wg:oauth:2.0:oob');}";
+echo "function exchange(){";
+echo "var xhr = new XMLHttpRequest();";
+echo "var cid_element = document.getElementById('cid');";
+echo "var cid_value = cid_element.value;";
+echo "var csec_value = document.getElementById('csec').value;";
+echo "var token_value = document.getElementById('tokenbox').value;";
+echo "xhr.open('POST', 'https://www.googleapis.com/oauth2/v3/token', true);";
+echo "xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');";
+echo "xhr.send('client_id='+cid_value+'&client_secret='+csec_value+'&code='+token_value+'&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob');";
+echo "xhr.onreadystatechange = function() {if (xhr.readyState == 4 && xhr.status == 200) {";
+echo "        var myArr = JSON.parse(xhr.responseText);";
+echo "	document.getElementById('refresh_token').value=myArr.refresh_token;}}}";
+echo "</script>";
 echo "<h2>"._("Google Voice (Motif)")."</h2>";
 
 $type = $action == 'add' ? 'Add' : 'Edit';
@@ -19,13 +46,24 @@ if($action == 'edit') {
             <tr>
                 <td colspan="2"><h5><?php echo _("Typical Settings")?></h5><hr></td>
             </tr>
+
+	<tr><td><a href="#" class="info">Get Authorization Token:<span>Get one-time authorization token from Google, a new window will open for you to authorize</span></a></td><td> <input type='checkbox' id='tokencb' name='authorize' id='authorize' onchange='open_auth_window()'></td></td><tr>
+	<td><a href="#" class="info">Authorization Token:<span>Enter authorization token provided by authorization window</span></a></td><td><input type='text' id='tokenbox' name='tokenbox' maxlength='100'></td></tr><tr><td><a href='#' onclick='exchange()'>Exchange authorization token for refresh token</a></td></tr>
             <tr>
                 <td><a href="#" class="info"><?php echo _("Google Voice Username")?>:<span><?php echo _("This is your google voice login.<br />If don't you supply '@domain' we will append @gmail.com")?></span></a></td>
                 <td><input type="text" name="username" maxlength="100" value="<?php echo isset($form_username) ? $form_username : ''; ?>"></td>
             </tr>
             <tr>
                 <td><a href="#" class="info"><?php echo _("Google Voice Password")?>:<span><?php echo _("This is your Google Voice Password")?></span></a></td>
-            	<td><input type="password" name="password" maxlength="150" value="<?php echo isset($form_password) ? $form_password : ''; ?>"></td>
+            	<td><input type="password" name="password" id="refresh_token" maxlength="150" value="<?php echo isset($form_password) ? $form_password : ''; ?>"></td>
+			</tr>
+            <tr>
+                <td><a href="#" class="info"><?php echo _("Google Voice OAUTH2.0 Client ID")?>:<span><?php echo _("This is your Google Voice OAUTH2.0 Client ID")?></span></a></td>
+            	<td><input type="text" name="client_id" id="cid" maxlength="150" value="<?php echo isset($form_client_id) ? $form_client_id : ''; ?>"></td>
+			</tr>
+            <tr>
+                <td><a href="#" class="info"><?php echo _("Google Voice OAUTH2.0 Client Secret")?>:<span><?php echo _("This is your Google Voice OAUTH2.0 Client Secret")?></span></a></td>
+            	<td><input type="text" name="client_secret" id='csec' maxlength="150" value="<?php echo isset($form_client_secret) ? $form_client_secret : ''; ?>"></td>
 			</tr>
 			<tr>
                 <td><a href="#" class="info"><?php echo _("Google Voice Phone Number")?>:<span><?php echo _("This is your Google Voice Phone Number <br />10 Digit Format")?></span></a></td>
@@ -39,6 +77,7 @@ if($action == 'edit') {
                 <td><a href="#" class="info"><?php echo _($type." Outbound Routes")?>:<span><?php echo _("Automatically Add Outbound Route for this Account")?></span></a></td>
             	<td><input type="checkbox" name="obroute" <?php echo isset($form_obroute) && $form_obroute ? 'CHECKED' : ''; ?>></td>
 			</tr>
+
 			<!-- not configured
 			<tr>
                 <td><a href="#" class="info"><?php echo _($type. " Inbound Routes")?>:<span><?php echo _("Automatically Add Inbound Routes for this Account")?></span></a></td>
